795273c3920128979964cead31de4eb510c63c30
sebbur0910 <sebbur0910@highgateschool.org.uk>
2023-07-19 13:10:09 +0100

    tidy and complete prototype

--- Prototypes/photo_upload/GUI.py
+++ Prototypes/photo_upload/GUI.py
@@ -1,14 +1,14 @@
-import PIL
 import PIL.Image
 import io
 import tkinter as tk
-import tkinter.ttk as ttk
 from tkinter.filedialog import askopenfile
 import sqlite3
 import binascii
 
+
 class UploadScreen(tk.Tk):
 
+#Sets up the button to be pressed to upload a file
     def __init__(self):
         super().__init__()
 
@@ -18,55 +18,52 @@ class UploadScreen(tk.Tk):
             command=self.open_file)
         self.button.pack()
 
-
+#The button calls this command, which grabs the file then saves it to the database
     def open_file(self):
-        file_path = askopenfile(mode='r', filetypes=[('Image Files', '*jpeg'),('Image Files', '*jpg'),('Image Files', '*png') ])
-        if file_path is not None:
-            pass
-        print(file_path.name)
+        file_path = askopenfile(mode='r',
+                                filetypes=[('Image Files', '*jpeg'), ('Image Files', '*jpg'), ('Image Files', '*png')])
+#Finds the name
+        name = ""
+        adding = False
+        for index in range(len(file_path.name) - 1, -1, -1):
+            character = file_path.name[index]
+            if character == "/":
+                break
+            elif adding == True:
+                name = character + name
+            elif character == ".":
+                adding = True
         with PIL.Image.open(file_path.name) as img:
+#Converts the image to 1s and 0s
             img.load()
-      #      print(img)
-            output = io.BytesIO()
-            img.save(output, format='JPEG')
-            hex_data = output.getvalue()
-  #          print(f"hex data: {hex_data}")
- #           self.save_to_database(hex_data)
+            bytestream = io.BytesIO()
+            img.save(bytestream, format='JPEG')
+            hex_data = bytestream.getvalue()
             data = binascii.hexlify(hex_data)
+            img = bin(int(data, 16))
+            img = img[2:].zfill(32)
 
-            binary = bin(int(data, 16))
-            binary = binary[2:].zfill(32)
-            print(binary)
-            self.save_to_database(binary)
+            self.save_to_database(name, img)
 
-    def save_to_database(self, img):
+    def save_to_database(self, photo_name, img):
         create_photos_table = """
-        CREATE TABLE photos (
+        CREATE TABLE IF NOT EXISTS photos (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name VARCHAR(30),
             photo BLOB
         )
         """
-        add_to_table = """
+        add_to_table = f"""
         INSERT INTO photos (name, photo)
-        VALUES (%s, %s);
+        VALUES ('{photo_name}', {img})
         """
 
         conn = sqlite3.connect("SQLite_Python.db")
         cursor = conn.cursor()
-        try:
-            cursor.execute(create_photos_table)
-        except:
-            pass
-  #      cursor.execute("""
-   #     INSERT INTO photos (name, photo)
-    #    VALUES ("ababb", 010100101010100100010)
-     #   """)
-        cursor.execute(f"""
-        INSERT INTO photos (name, photo)
-        VALUES ("name", {img})
-        """)
+        cursor.execute(create_photos_table)
+        cursor.execute(add_to_table)
         conn.commit()
+
 
 gui = UploadScreen()
 gui.mainloop()
