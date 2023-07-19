6e6c0dd9f257c31e7ed3979c3a0478a943bab628
sebbur0910 <sebbur0910@highgateschool.org.uk>
2023-07-19 12:39:48 +0100

    prototype with all the mess

--- Prototypes/photo_upload/GUI.py
+++ Prototypes/photo_upload/GUI.py
@@ -1,7 +1,11 @@
 import PIL
+import PIL.Image
+import io
 import tkinter as tk
 import tkinter.ttk as ttk
 from tkinter.filedialog import askopenfile
+import sqlite3
+import binascii
 
 class UploadScreen(tk.Tk):
 
@@ -22,7 +26,47 @@ class UploadScreen(tk.Tk):
         print(file_path.name)
         with PIL.Image.open(file_path.name) as img:
             img.load()
+      #      print(img)
+            output = io.BytesIO()
+            img.save(output, format='JPEG')
+            hex_data = output.getvalue()
+  #          print(f"hex data: {hex_data}")
+ #           self.save_to_database(hex_data)
+            data = binascii.hexlify(hex_data)
 
+            binary = bin(int(data, 16))
+            binary = binary[2:].zfill(32)
+            print(binary)
+            self.save_to_database(binary)
+
+    def save_to_database(self, img):
+        create_photos_table = """
+        CREATE TABLE photos (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(30),
+            photo BLOB
+        )
+        """
+        add_to_table = """
+        INSERT INTO photos (name, photo)
+        VALUES (%s, %s);
+        """
+
+        conn = sqlite3.connect("SQLite_Python.db")
+        cursor = conn.cursor()
+        try:
+            cursor.execute(create_photos_table)
+        except:
+            pass
+  #      cursor.execute("""
+   #     INSERT INTO photos (name, photo)
+    #    VALUES ("ababb", 010100101010100100010)
+     #   """)
+        cursor.execute(f"""
+        INSERT INTO photos (name, photo)
+        VALUES ("name", {img})
+        """)
+        conn.commit()
 
 gui = UploadScreen()
 gui.mainloop()
