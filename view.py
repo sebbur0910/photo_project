36a0251896c1f298db348671510029aedb45c49f
sebbur0910 <sebbur0910@highgateschool.org.uk>
2023-11-08 12:59:59 +0000

    some easy GUI stuff made

--- view.py
+++ view.py
@@ -22,9 +22,12 @@ class App(ctk.CTk):
     def show_frame(self, current_frame):
         self.frames = {
             "homescreen": HomeScreen(self),
-            "add_timeline": AddTimeline(self),
-            "photo_gallery": PhotoGallery(self),
-            "timeline": TimelineView(self)
+            "add_timeline": CustomiseTimeline(self, True),
+            "photo_gallery": PhotoGallery(self, False),
+            "timeline": TimelineView(self),
+            "customise_timeline": CustomiseTimeline(self, False),
+            "timeline_photos": PhotoGallery(self, True),
+            "timeline_new_photo": ImportPhoto(self)
         }
         widgets = self.winfo_children()
         for widget in widgets:
@@ -32,7 +35,7 @@ class App(ctk.CTk):
                 widget.pack_forget()
         frame_to_show = self.frames[current_frame]
         frame_to_show.pack(expand=True, fill=ctk.BOTH)
-     #   frame_to_show.set_up()
+    #   frame_to_show.set_up()
 
 
 class HomeScreen(ctk.CTkFrame):
@@ -44,8 +47,8 @@ class HomeScreen(ctk.CTkFrame):
         plus_image = database.get_image("plus")
         self.my_image = ctk.CTkImage(light_image=Image.open(plus_image),
                                      size=(150, 100))
-  #      self.configure(background="white")
-   #     self.title("Home")
+        #      self.configure(background="white")
+        #     self.title("Home")
 
         self.title_text = ctk.CTkLabel(self,
                                        text="Home",
@@ -68,7 +71,6 @@ class HomeScreen(ctk.CTkFrame):
 
         self.thumbnails = database.get_thumbnails()
 
-
         self.place()
 
     def place(self):
@@ -76,9 +78,9 @@ class HomeScreen(ctk.CTkFrame):
         self.sort_by_label.grid(row=1, column=0)
         self.sorter.grid(row=1, column=1)
         self.add_new_thumbnail.grid(row=2, column=1)
-        self.place_timelines()
+        self.place_timelines("")
 
-    def place_timelines(self,sorted_ids):
+    def place_timelines(self, sorted_ids):
         for id in sorted_ids:
             thumbnail = database.get_thumbnail(id)
             thumbnail = ctk.CTkImage(light_image=Image.open(thumbnail),
@@ -92,6 +94,7 @@ class HomeScreen(ctk.CTkFrame):
 
     def open_timeline(self):
         ...
+
     def sort_request(self, factor):
         self.place_timelines(database.sort_timelines(factor))
 
@@ -99,19 +102,209 @@ class HomeScreen(ctk.CTkFrame):
         self.root.show_frame("add_timeline")
 
 
-class AddTimeline(ctk.CTkFrame):
-    def __init__(self, root):
+class CustomiseTimeline(ctk.CTkFrame):
+    def __init__(self, root, new: bool):
+        super().__init__(root)
+        self.root = root
+        if new:
+            title_text = "Create new timeline"
+        else:
+            title_text = None
+
+        self.title_text = ctk.CTkLabel(self,
+                                       text=title_text,
+                                       font=("Arial Bold", 35),
+                                       pady=20
+                                       )
+
+        self.name_text = ctk.CTkLabel(self,
+                                      text="Name:",
+                                      font=("Arial", 20),
+                                      anchor="e"
+                                      )
+
+        self.name_box = ctk.CTkEntry(self,
+                                     bg_color="grey",
+                                     width=300)
+
+        self.line_colour_text = ctk.CTkLabel(self,
+                                             text="Line colour:",
+                                             font=("Arial", 20),
+                                             anchor="e"
+                                             )
+
+        self.line_colour_box = ctk.CTkEntry(self,
+                                            bg_color="grey",
+                                            width=300)
+
+        self.line_weight_text = ctk.CTkLabel(self,
+                                             text="Line weight:",
+                                             font=("Arial", 20),
+                                             anchor="e"
+                                             )
+
+        self.line_weight_box = ctk.CTkEntry(self,
+                                            bg_color="grey",
+                                            width=300)
+
+        self.default_border_colour_text = ctk.CTkLabel(self,
+                                                       text="Default border colour:",
+                                                       font=("Arial", 20),
+                                                       anchor="e"
+                                                       )
+
+        self.default_border_colour_box = ctk.CTkEntry(self,
+                                                      bg_color="grey",
+                                                      width=300)
+
+        self.default_border_weight_text = ctk.CTkLabel(self,
+                                                       text="Default border weight:",
+                                                       font=("Arial", 20),
+                                                       anchor="e"
+                                                       )
+
+        self.default_border_weight_box = ctk.CTkEntry(self,
+                                                      bg_color="grey",
+                                                      width=300)
+
+        self.background_colour_text = ctk.CTkLabel(self,
+                                                   text="Background colour:",
+                                                   font=("Arial", 20),
+                                                   anchor="e"
+                                                   )
+
+        self.background_colour_box = ctk.CTkEntry(self,
+                                                  bg_color="grey",
+                                                  width=300)
+
+        self.photos_text = ctk.CTkLabel(self,
+                                        text="Photos:",
+                                        font=("Arial", 30))
+
+        self.view_current_button = ctk.CTkButton(self,
+                                                 text="View/delete current",
+                                                 font=("Arial", 30),
+                                                 width=350,
+                                                 height=150,
+                                                 command=self.view_current
+                                                 )
+
+        self.insert_existing_button = ctk.CTkButton(self,
+                                                    text="Insert from photo gallery",
+                                                    font=("Arial", 30),
+                                                    width=350,
+                                                    height=150,
+                                                    command=self.insert_existing
+                                                    )
+
+        self.insert_new_button = ctk.CTkButton(self,
+                                               text="Insert new",
+                                               font=("Arial", 30),
+                                               width=350,
+                                               height=150,
+                                               command=self.insert_new
+                                               )
+
+        self.save_button = ctk.CTkButton(self,
+                                         text="Save",
+                                         font=("Arial", 15),
+                                         corner_radius=3,
+                                         command=self.save)
+
+        self.place()
+
+    def save(self):
         ...
+        # update database
+        # go back to original screen (parameterised exit)
+
+    def view_current(self):
+        self.root.show_frame("timeline_photos")
+
+    def insert_existing(self):
+        self.root.show_frame("photo_gallery")
+
+    def insert_new(self):
+        self.root.show_frame("timeline_new_photo")
+
+    def place(self):
+        self.title_text.grid(row=0, column=1)
+        self.name_text.grid(row=1, column=0, sticky="e", padx=10, pady=5)
+        self.name_box.grid(row=1, column=1)
+        self.line_colour_text.grid(row=2, column=0, sticky="e", padx=10, pady=5)
+        self.line_colour_box.grid(row=2, column=1)
+        self.line_weight_text.grid(row=3, column=0, sticky="e", padx=10, pady=5)
+        self.line_weight_box.grid(row=3, column=1)
+        self.default_border_colour_text.grid(row=4, column=0, sticky="e", padx=10, pady=5)
+        self.default_border_colour_box.grid(row=4, column=1)
+        self.default_border_weight_text.grid(row=5, column=0, sticky="e", padx=10, pady=5)
+        self.default_border_weight_box.grid(row=5, column=1)
+        self.background_colour_text.grid(row=6, column=0, sticky="e", padx=10, pady=5)
+        self.background_colour_box.grid(row=6, column=1)
+        self.photos_text.grid(row=7, column=0, pady=20)
+        self.view_current_button.grid(row=8, column=0, pady=10, padx=20)
+        self.insert_existing_button.grid(row=8, column=1, pady=10, padx=20)
+        self.insert_new_button.grid(row=8, column=2, pady=10, padx=20)
+        self.save_button.grid(row=9, column=2, sticky="e")
 
 
 class PhotoGallery(ctk.CTkFrame):
-    def __init__(self, root):
+    def __init__(self, root, master_filter):
         ...
 
 
 class TimelineView(ctk.CTkFrame):
     def __init__(self, root):
         ...
+
+class ImportPhoto(ctk.CTkFrame):
+
+    def __init__(self, root):
+        super().__init__(root)
+        self.root = root
+
+        self.title_text = ctk.CTkLabel(self,
+                                       text="Import photo",
+                                       font=("Arial Bold", 35),
+                                       pady=20
+                                       )
+
+        self.date_taken_text = ctk.CTkLabel(self,
+                                      text="Date taken:",
+                                      font=("Arial", 20),
+                                      anchor="e"
+                                      )
+
+        self.date_taken_box = ctk.CTkEntry(self,
+                                     bg_color="grey",
+                                     width=300)
+
+        self.caption_text = ctk.CTkLabel(self,
+                                             text="Caption:",
+                                             font=("Arial", 20),
+                                             anchor="e"
+                                             )
+
+        self.caption_box = ctk.CTkEntry(self,
+                                            bg_color="grey",
+                                            width=300)
+
+        self.upload_button = ctk.CTkButton(self,
+                                           text="Upload",
+                                           font=("Arial Bold", 35),
+                                           width=700,
+                                           height=300
+        )
+
+        self.place()
+
+    def place(self):
+        self.title_text.grid(row=0, column=1)
+        self.date_taken_text.grid(row=1, column=0, sticky="e", padx=10, pady=5)
+        self.date_taken_box.grid(row=1, column=1)
+        self.caption_text.grid(row=2, column=0, sticky="e", padx=10, pady=5)
+        self.caption_box.grid(row=2, column=1)
+        self.upload_button.grid(row=3, column=0, columnspan=3, padx=60, pady=60)
 
 
 gui = App()
